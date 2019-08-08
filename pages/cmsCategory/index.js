eb22544866e23f59e00d8d9e673b907d633f054e
gooking <javazj@qq.com>
2019-08-08 09:05:03 +0800

    增加cms文章分类演示程序

---
+++ pages/cmsCategory/index.js
@@ -0,0 +1,47 @@
+const WXAPI = require('apifm-wxapi')
+WXAPI.init('gooking')
+
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+
+  },
+  onShow: function () {
+
+  },
+  cmsCategories() { // 读取所有的分类数据
+    WXAPI.cmsCategories().then(res => {
+      console.log(res)
+      if (res.code == 0) {
+        wx.showToast({
+          title: '读取成功',
+          icon: 'success'
+        })
+      } else {
+        wx.showToast({
+          title: res.msg,
+          icon: 'none'
+        })
+      }
+    })
+  },
+  cmsCategoryDetail() { // 读取某个分类的详情数据
+    const cmsCategoryId = 3189 // 记录中的记录ID
+    WXAPI.cmsCategoryDetail(cmsCategoryId).then(res => {
+      console.log(res)
+      if (res.code == 0) {
+        wx.showToast({
+          title: '读取成功',
+          icon: 'success'
+        })
+      } else {
+        wx.showToast({
+          title: res.msg,
+          icon: 'none'
+        })
+      }
+    })
+  },
+})
